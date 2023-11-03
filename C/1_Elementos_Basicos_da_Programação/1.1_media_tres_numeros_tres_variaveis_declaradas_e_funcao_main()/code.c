fd129113f0393a4917091e74c90cc5d784df4911
Gesiel Junio <gesiel.gigabyte@gmail.com>
2023-11-02 23:41:39 -0300

    more codes

---
+++ C/1_Elementos_Basicos_da_Programação/1.1_media_tres_numeros_tres_variaveis_declaradas_e_funcao_main()/code.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+ main(){
+
+    float a;
+    float b;
+    float c;
+
+    return 0;
+}
---
+++ C/1_Elementos_Basicos_da_Programação/1.3_lendo_tipos_de_variaveis/lendo_tres_variaveis_com_unico_scanf/code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main (){
+    int a;
+    float b;
+    double c;
+
+    scanf("%d,%f,%lf", &a, &b, &c);
+
+    return 0;
+
+}
