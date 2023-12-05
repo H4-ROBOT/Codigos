a5b72e08fec076c6159a4046521876cecc6dc5da
Gesiel Junio <gesiel.gigabyte@gmail.com>
2023-12-05 10:07:59 -0300

    Att. pastas

---
+++ C/Praticando/IF_&_Operadores relacionais/exemplo.c
@@ -0,0 +1,26 @@
+/*Operadores Lógicos e relacionais 
+
+Maior : (>)
+Maior ou Igual : (>=)
+Menor : (<)
+Menor ou Igual : (<=)
+Igual : (==)
+Diferente :(!=)
+
+If = Se 
+*/
+
+#include <stdio.h>
+
+int main(){
+
+float m;
+
+printf("Insira uma nota:\n");
+scanf("%f", &m);
+
+if(m >= 7.0){
+    printf("Aprovado(a)!\n");
+}
+
+}
