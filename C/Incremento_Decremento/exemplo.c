4263dd0348b681c4072f989ec38998d9fe1acdc6
Gesiel Junio <gesiel.gigabyte@gmail.com>
2023-12-05 09:51:47 -0300

    atualização de Incremento e Decremento
    Foi realizado alguns exemplos claros de como usar e o que acontece se usa-los na seguinte sequencia de ++,--,+=(valor),-=(valor), *=(valor).

---
+++ C/Incremento_Decremento/exemplo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+    int dado = 10;
+
+    printf("Dado antes do incremento: %d.\n", dado);
+
+    dado++;
+  
+    printf("dado++;  \nDepois do incremento:%d.\n", dado);
+
+    dado--;
+
+    printf("dado--;  \nDepois do decremento:%d.\n", dado);
+
+    dado+=3;
+
+    printf("dado+=3;  \nDepois do incremento em 3 unidades :%d.\n", dado);
+
+    dado-=2;
+
+    printf("dado-=2;  \nDepois do decremento em 2 unidades :%d.\n", dado);
+
+    dado *=10;
+
+     printf("dado *=10;  \nDepois de multiplicar por 10 :%d.\n", dado);
+
+
+}
